68d817e23ac33199fca7b192cf081985d52aee51
binarystarcs <39010956+binarystarcs@users.noreply.github.com>
2023-11-28 18:52:08 +0000

    Added all four operations of negative number arithmetic

--- questions/negative_arithmetic.py
+++ questions/negative_arithmetic.py
@@ -14,11 +14,69 @@ from engine.generate import QuestionGenerator
 
 
 @QuestionGenerator.register(TOPIC_NEGATIVE_ARITHMETIC, 1)
-def generate_level_one_negative_plus_positive():
-    negative_value = random.randint(-10, -1)
-    positive_value = random.randint(1, 10)
-    statement = f"Work out {negative_value} + {positive_value}"
-    answer = negative_value + positive_value
+def generate_level_one_negative_addition():
+    first_value = 1
+    second_value = 1
+    while first_value >= 0 and second_value >= 0:
+        first_value = random.randint(-10, 10)
+        second_value = random.randint(-10, 10)
+    statement = f"Work out {first_value} + {second_value}"
+    answer = first_value + second_value
+    return {
+        "statement": statement,
+        "answer": str(answer),
+        "display": DISPLAY_STANDARD_LATEX,
+        "answer_format": ANSWER_FORMAT_INTEGER,
+        "verifier": VERIFIER_EXACT,
+    }
+
+
+@QuestionGenerator.register(TOPIC_NEGATIVE_ARITHMETIC, 1)
+def generate_level_one_negative_subtraction():
+    first_value = 1
+    second_value = 1
+    while first_value >= 0 and second_value >= 0:
+        first_value = random.randint(-10, 10)
+        second_value = random.randint(-10, 10)
+    statement = f"Work out {first_value} - {second_value}"
+    answer = first_value - second_value
+    return {
+        "statement": statement,
+        "answer": str(answer),
+        "display": DISPLAY_STANDARD_LATEX,
+        "answer_format": ANSWER_FORMAT_INTEGER,
+        "verifier": VERIFIER_EXACT,
+    }
+
+
+@QuestionGenerator.register(TOPIC_NEGATIVE_ARITHMETIC, 1)
+def generate_level_one_negative_multiplication():
+    first_value = 1
+    second_value = 1
+    while first_value >= 0 and second_value >= 0:
+        first_value = random.randint(-10, 10)
+        second_value = random.randint(-10, 10)
+    statement = f"Work out {first_value} \\times {second_value}"
+    answer = first_value * second_value
+    return {
+        "statement": statement,
+        "answer": str(answer),
+        "display": DISPLAY_STANDARD_LATEX,
+        "answer_format": ANSWER_FORMAT_INTEGER,
+        "verifier": VERIFIER_EXACT,
+    }
+
+
+@QuestionGenerator.register(TOPIC_NEGATIVE_ARITHMETIC, 1)
+def generate_level_one_negative_division():
+    first_value = 1
+    second_value = 1
+    while (first_value >= 0 and second_value >= 0) or first_value * second_value == 0:
+        first_value = random.randint(-6, 6)
+        second_value = random.randint(-6, 6)
+    product = first_value * second_value
+    statement = f"Work out {product} \\div {second_value}"
+    answer = first_value
     return {
         "statement": statement,
         "answer": str(answer),
