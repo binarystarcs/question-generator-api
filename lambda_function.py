0cc72a86b48a723bbb66d88bc4450159300bd528
binarystarcs <39010956+binarystarcs@users.noreply.github.com>
2023-12-09 13:33:23 +0000

    Added CORS headers to lambda response

--- lambda_function.py
+++ lambda_function.py
@@ -4,16 +4,29 @@ import getter
 import logging
 
 
+def form_response(data):
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+            "Access-Control-Allow-Methods": "OPTIONS, POST",
+        },
+        "body": json.dumps(data),
+    }
+    return response
+
+
 def lambda_handler(event, context):
     if event["rawPath"] == "/question":
         logging.info("Handling question request:")
         try:
             body = json.loads(event["body"])
             question = getter.handler(body)
-            return json.dumps(question)
+            return form_response(question)
         except:
             logging.error("Server error")
-            return json.dumps({"error": "Server error"})
+            return form_response({"error": "Server error"})
     elif event["rawPath"] == "/answer":
         logging.info("Handling answer check request:")
         logging.info(event)
@@ -21,8 +34,8 @@ def lambda_handler(event, context):
             body = json.loads(event["body"])
             result = checker.handler(body)
             if result is None:
-                return json.dumps({"error": "No question generated"})
-            return json.dumps(result)
+                return form_response({"error": "No question generated"})
+            return form_response(result)
         except:
             logging.error("Server error")
-            return json.dumps({"error": "Server error"})
+            return form_response({"error": "Server error"})
